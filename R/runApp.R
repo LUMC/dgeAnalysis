f81d70f8e686485cb0521413379249c7996a9066
tomkuipers1402 <t.b.kuipers@outlook.com>
2020-03-25 16:32:53 +0100

    Rewrite start up application

--- R/runApp.R
+++ R/runApp.R
@@ -1,45 +1,23 @@
 #' @export
-#' @return Shiny application.
-#' @import shiny
-#' @import shinydashboard
-#' @import shinyWidgets
-#' @import shinycssloaders
-#' @import shinyjs
-#' @import BiocManager
-#' @import knitr
-#' @import SummarizedExperiment
-#' @import edgeR
-#' @import limma
-#' @import DESeq2
-#' @import tidyr
-#' @import scales
-#' @import broom
-#' @import plotly
-#' @import clusterProfiler
-#' @import DOSE
-#' @import graphite
-#' @import ReactomePA
-#' @import igraph
-#' @import org.Hs.eg.db
-#' @import org.Mm.eg.db
-#' @import reshape2
-#' 
-#' #' @examples
-#' if ( interactive() ) {
-#'   startApp()
-#' }
+
 startApp <- function() {
-  source(system.file("shiny/ui_main.R", package = "dgeAnalysis"), local = TRUE)
-  source(system.file("shiny/svr_main.R", package = "dgeAnalysis"), local = TRUE)
-  source(system.file("scripts/de.R", package = "dgeAnalysis"), local = TRUE)
-  source(system.file("scripts/enrichment.R", package = "dgeAnalysis"), local = TRUE)
-  source(system.file("scripts/markdown.R", package = "dgeAnalysis"), local = TRUE)
-  source(system.file("scripts/plots.R", package = "dgeAnalysis"), local = TRUE)
+  appDir <- system.file("src", package = "dgeAnalysis")
+  
+  if (appDir == "") {
+    stop("Could not find 'dgeAnalysis'. Try re-installing 'dgeAnalysis'.", call. = FALSE)
+  }
   
   options(shiny.maxRequestSize = 50*1024^2)
-  options(spinner.color="#0088cc")
-  # suppressWarnings(rm(list=c("deTab", "normDge", "inUse_deTab", "inUse_normDge"), envir=.GlobalEnv))
-  # options(warn = -1)
+  options(spinner.color = "#0088cc")
+  options(warn = -1)
   
-  shiny::shinyApp(ui = ui, server = server, options=list(host="0.0.0.0", port=1402, launch.browser=TRUE))
+  suppressWarnings(rm(list=c("deTab", "normDge", "inUse_deTab", "inUse_normDge"), envir=.GlobalEnv))
+  suppressMessages(source(system.file("src/scripts/libraries.R", package = "dgeAnalysis")))
+  
+  source(system.file("src/scripts/de.R", package = "dgeAnalysis"), local = TRUE)
+  source(system.file("src/scripts/enrichment.R", package = "dgeAnalysis"), local = TRUE)
+  source(system.file("src/scripts/markdown.R", package = "dgeAnalysis"), local = TRUE)
+  source(system.file("src/scripts/plots.R", package = "dgeAnalysis"), local = TRUE)
+  
+  shiny::runApp(appDir, host="0.0.0.0", port=1402, launch.browser=TRUE)
 }
--- inst/app.R
+++ inst/app.R
@@ -1,15 +1,15 @@
 
-source("scripts/libraries.R", local = TRUE)
-source("shiny/ui_main.R", local = TRUE)
-source("shiny/svr_main.R", local = TRUE)
-source("scripts/de.R", local = TRUE)
-source("scripts/enrichment.R", local = TRUE)
-source("scripts/markdown.R", local = TRUE)
-source("scripts/plots.R", local = TRUE)
+## USE ONLY FOR DEVELOPMENT!!!
 
 options(shiny.maxRequestSize = 50*1024^2)
 options(spinner.color="#0088cc")
 # suppressWarnings(rm(list=c("deTab", "normDge", "inUse_deTab", "inUse_normDge"), envir=.GlobalEnv))
-# options(warn = -1)
+options(warn = -1)
 
-shiny::shinyApp(ui = ui, server = server)
+source("inst/src/scripts/libraries.R", local = TRUE)
+source("inst/src/scripts/de.R", local = TRUE)
+source("inst/src/scripts/enrichment.R", local = TRUE)
+source("inst/src/scripts/markdown.R", local = TRUE)
+source("inst/src/scripts/plots.R", local = TRUE)
+
+shiny::runApp("inst/src/", host="0.0.0.0", port=1402, launch.browser=TRUE)
