c51acf8e194d8d2ce371d447b92be5e66808de68
tomkuipers1402 <t.b.kuipers@outlook.com>
2020-10-28 12:19:10 +0100

    reformat code in app start script for development

--- inst/app.R
+++ inst/app.R
@@ -1,8 +1,8 @@
 
 ## USE ONLY FOR DEVELOPMENT!!!
 
-options(shiny.maxRequestSize = 50*1024^2)
-options(spinner.color="#0088cc")
+options(shiny.maxRequestSize = 50 * 1024 ^ 2)
+options(spinner.color = "#0088cc")
 # suppressWarnings(rm(list=c("deTab", "normDge", "inUse_deTab", "inUse_normDge"), envir=.GlobalEnv))
 options(warn = -1)
 
@@ -13,4 +13,9 @@ source("R/markdown.R", local = TRUE)
 source("R/de_plots.R", local = TRUE)
 source("R/wcgna.R", local = TRUE)
 
-shiny::runApp("inst/src/", host="0.0.0.0", port=1402, launch.browser=TRUE)
+shiny::runApp(
+  "inst/src/",
+  host = "0.0.0.0",
+  port = 1402,
+  launch.browser = TRUE
+)
