3a5e07bf6fb3d260a872a25c0b32ff394f37cfc1
tomkuipers1402 <t.b.kuipers@outlook.com>
2020-10-28 12:19:37 +0100

    reformat code in main ui and server

--- inst/src/server.R
+++ inst/src/server.R
@@ -2,7 +2,6 @@
 options(warn = -1)
 
 server <- function(input, output, session) {
-  
   ## Check if objects need to reload
   checkReload <- function() {
     is.null(input$app_mode)
@@ -54,34 +53,101 @@ server <- function(input, output, session) {
   
   ## Render navigation bar to ui
   output[["sidebar_tabs"]] <- renderMenu({
-    sidebarMenu(id = "sidebar",
-      menuItem("Home", tabName = "home",icon = icon("home")),
-      menuItem("Data upload", tabName = "upload", icon = icon("upload")),
-      menuItem("Run Analysis", tabName = "run_analysis", icon = icon("calculator")),
-      menuItem("Pre-Analysis", icon = icon("clipboard-check"),
-               menuItem("Alignment", tabName = "alignment", icon = icon("chart-line")),
-               menuItem("Raw Data", tabName = "raw_data", icon = icon("chart-line"))
+    sidebarMenu(
+      id = "sidebar",
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      menuItem(
+        "Data upload",
+        tabName = "upload",
+        icon = icon("upload")
       ),
-      menuItem("Analysis", icon = icon("binoculars"),
-               menuItem("Normalization", tabName = "norm_data", icon = icon("chart-line")),
-               menuItem("PCA", tabName = "pca", icon = icon("chart-line")),
-               menuItem("Heatmaps", tabName = "heatmaps", icon = icon("chart-line")),
-               menuItem("DE analysis", tabName = "analysis", icon = icon("chart-line")),
-               menuItem("Bias", tabName = "bias", icon = icon("chart-line"))
+      menuItem(
+        "Run Analysis",
+        tabName = "run_analysis",
+        icon = icon("calculator")
       ),
-      menuItem("Enrichment", icon = icon("bezier-curve"),
-               menuSubItem("KEGG", tabName = "enrich_kegg", icon = icon("bezier-curve")),
-               menuSubItem("Reactome", tabName = "enrich_reactome", icon = icon("bezier-curve")),
-               menuSubItem("Gene Ontology", tabName = "enrich_go", icon = icon("bezier-curve")),
-               menuSubItem("Disease Ontology", tabName = "enrich_do", icon = icon("bezier-curve"))
+      menuItem(
+        "Pre-Analysis",
+        icon = icon("clipboard-check"),
+        menuItem(
+          "Alignment",
+          tabName = "alignment",
+          icon = icon("chart-line")
+        ),
+        menuItem(
+          "Raw Data",
+          tabName = "raw_data",
+          icon = icon("chart-line")
+        )
       ),
-      menuItem("WGCNA", tabName = "wgcna", icon = icon("code-branch")),
-      menuItem("Export", tabName = "export", icon = icon("download")),
-      menuItem("About", tabName = "about", icon = icon("info-circle")),
-      img(src = "lumcLogo.png", width="200px")
+      menuItem(
+        "Analysis",
+        icon = icon("binoculars"),
+        menuItem(
+          "Normalization",
+          tabName = "norm_data",
+          icon = icon("chart-line")
+        ),
+        menuItem("PCA", tabName = "pca", icon = icon("chart-line")),
+        menuItem(
+          "Heatmaps",
+          tabName = "heatmaps",
+          icon = icon("chart-line")
+        ),
+        menuItem(
+          "DE analysis",
+          tabName = "analysis",
+          icon = icon("chart-line")
+        ),
+        menuItem(
+          "Bias",
+          tabName = "bias",
+          icon = icon("chart-line")
+        )
+      ),
+      menuItem(
+        "Enrichment",
+        icon = icon("bezier-curve"),
+        menuSubItem(
+          "KEGG",
+          tabName = "enrich_kegg",
+          icon = icon("bezier-curve")
+        ),
+        menuSubItem(
+          "Reactome",
+          tabName = "enrich_reactome",
+          icon = icon("bezier-curve")
+        ),
+        menuSubItem(
+          "Gene Ontology",
+          tabName = "enrich_go",
+          icon = icon("bezier-curve")
+        ),
+        menuSubItem(
+          "Disease Ontology",
+          tabName = "enrich_do",
+          icon = icon("bezier-curve")
+        )
+      ),
+      menuItem(
+        "WGCNA",
+        tabName = "wgcna",
+        icon = icon("code-branch")
+      ),
+      menuItem(
+        "Export",
+        tabName = "export",
+        icon = icon("download")
+      ),
+      menuItem(
+        "About",
+        tabName = "about",
+        icon = icon("info-circle")
+      ),
+      img(src = "lumcLogo.png", width = "200px")
     )
   })
-
+  
   
   source("tabs/home/svr_home.R", local = TRUE)
   source("tabs/upload/svr_upload.R", local = TRUE)
@@ -100,5 +166,5 @@ server <- function(input, output, session) {
   source("tabs/wgcna/svr_wgcna.R", local = TRUE)
   source("tabs/export/svr_export.R", local = TRUE)
   source("tabs/about/svr_about.R", local = TRUE)
-
+  
 }
